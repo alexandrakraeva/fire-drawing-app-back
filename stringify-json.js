2e8ff2e2545f3c3fe0ecf3110a034a911362551a
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2024-01-15 13:24:14 +0100

    df

---
+++ stringify-json.js
@@ -0,0 +1,5 @@
+﻿// Load the JSON data into a variable
+const jsonData = require('./fifth-being-411122-73c1efffad5b.json');
+// Stringify the JSON data
+const jsonString = JSON.stringify(jsonData);
+console.log(jsonString); 
