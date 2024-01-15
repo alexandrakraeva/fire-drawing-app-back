cc85b03447d3729aa782f5c1e00126eca12ef4bb
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2024-01-15 12:09:52 +0100

    sd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 ﻿const express = require('express');
 const bodyParser = require('body-parser');
-const { Storage } = require('@google-cloud/storage');
+
 const app = express();
 
 const PORT = 3000; // Define port 3000
